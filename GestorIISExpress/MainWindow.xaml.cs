53bb219205be151edfbdb6fb638d9b0ca4ba75da
Fernando David Minguela Pablos <Fernando David Minguela Pablos>
2016-06-24 14:25:39 +0200

    Primer guardado

---
+++ GestorIISExpress/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace GestorIISExpress
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private XMLConfiguracion archivoConfiguracion { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            archivoConfiguracion = new XMLConfiguracion();
+            archivoConfiguracion.CargarXML();
+
+            foreach (XElement sitio in archivoConfiguracion.xmlSitios)
+            {
+                var nodo = new TreeViewItem()
+                {
+                    Header = sitio.Attribute("name").Value,
+                    Tag = sitio.Attribute("id").Value
+                };
+
+                nodo.Selected += nodo_Selected;
+
+                NodoSitios.Items.Add(nodo);
+            }
+
+        }
+
+        void nodo_Selected(object sender, RoutedEventArgs e)
+        {
+            var nodo = archivoConfiguracion.xmlSitios.Where(z => z.Attribute("id").Value == (sender as TreeViewItem).Tag.ToString()).FirstOrDefault();
+            if (nodo != null)
+            {
+                txtNombre.Text = nodo.Attribute("name").Value.ToString();
+                txtID.Text = string.Format("Id: {0}", nodo.Attribute("id").Value.ToString());
+                txtPuerto.Text = nodo.Elements("bindings").Elements("binding").First().Attribute("bindingInformation").Value.ToString().Replace("*:","").Replace(":localhost","");
+                var apps = nodo.Elements("application");
+
+                List<XMLConfiguracion.Applicacion> aplicaciones = new List<XMLConfiguracion.Applicacion>();
+                foreach (var app in apps)
+                {
+                    aplicaciones.Add(
+                    new XMLConfiguracion.Applicacion()
+                    {
+                        Path = app.Attribute("path").Value.ToString(),
+                        PhysicalPath = app.Elements("virtualDirectory").First().Attribute("physicalPath").Value.ToString()
+                    });
+                }
+
+                dtApps.ItemsSource = aplicaciones;
+            }
+        }
+    }
+}
---
+++ GestorIISExpress/XMLConfiguracion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GestorIISExpress
+{
+    /// <summary>
+    /// Control del archivo de configuracion y de su gestion
+    /// </summary>
+    public class XMLConfiguracion
+    {
+        private static string RUTA_XML_CONFIGURACION = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"IISExpress\config\applicationhost.config");
+        private XDocument xmlFile = null;
+        public List<XElement> xmlSitios { get; set; }
+
+
+        public void CargarXML()
+        {
+
+            xmlFile = XDocument.Load(RUTA_XML_CONFIGURACION);
+            var sitios = xmlFile.Elements("configuration").Elements("system.applicationHost").Elements("sites").Elements("site");
+
+            if (sitios != null)
+            {
+                xmlSitios = sitios.ToList();
+            }
+
+            Console.WriteLine("Se han cargado los sitios");
+            /*var query = from c in xmlFile.Elements("catalog").Elements("book")
+                        select c;
+            foreach (XElement book in query)
+            {
+                book.Attribute("attr1").Value = "MyNewValue";
+            }
+            xmlFile.Save("books.xml");*/      
+        }
+
+        public class Applicacion
+        {
+            public string Path { get; set; }
+            public string PhysicalPath { get; set; }
+        }
+
+    }
+}
